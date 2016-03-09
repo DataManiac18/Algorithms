ca414de9caea12b04af813701218915f9dbc54b5
DataManiac18 <datamaniac18@gmail.com>
2016-03-09 12:59:20 -0500

    Added an awesome new println!!
    yep

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
 	public static void main(String[] args) {
 		int pi = 3;
 		System.out.println("Pi is equal to " + pi + ". Everyone knows that.");
+		System.out.println("This next number is insignificant: " + 4*pi);
 	}
 }
